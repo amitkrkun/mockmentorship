8699319bb8e26ad8e2807d376c00d2dd5a7bbc5e
amitkrkun <amitkrsh.1999@gmail.com>
2024-06-11 11:01:58 +0530

    udpdate

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -4,7 +4,7 @@ const { default: axios } = require("axios");
 const API_KEY=process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient=axios.create({
-    baseURL:'http://localhost:1337/api',
+    baseURL:'https://mockmentorship-admin.onrender.com/api',
     headers:{
         'Authorization':`Bearer ${API_KEY}`
     }
